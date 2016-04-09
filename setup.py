a8e3b78b334a63900176034a9902ce44ae61ddc7
avi <hi@avi.im>
2016-04-09 17:10:32 +0530

    Add `requirements.txt` file and bump up version in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
 
 setup(name='Isso OpenShift',
-      version='0.1',
+      version='0.2',
       description='This kit helps you install Isso on OpenShift with single click',
       author='Avinash Sajjanshetty',
       author_email='hi@avi.im',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['isso==0.10.3', 'gunicorn==19.4.5', 'passlib==1.6.5'],)
+      url='https://github.com/avinassh/isso-openshift/',)
